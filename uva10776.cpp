095e0f70afa99274c382c7f53a94d95d8eb6c52f
SunTalk <infernal56727@gmail.com>
2018-10-01 19:58:35 +0800

    add 12908 ac

--- uva10776.cpp
+++ uva10776.cpp
@@ -1,16 +1,52 @@
 #include <bits/stdc++.h>
-// http://luckycat.kshs.kh.edu.tw/homework/q10776.htm
+
 using namespace std;
 
 #define TESTC ""
 #define PROBLEM "10776"
 
+#define USE_CPPIO() ios_base::sync_with_stdio(0); cin.tie(0)
+
 int main()
 {
 	#ifdef DBG
-	freopen(PROBLEM TESTC ".in", "r", stdin);
-	freopen(PROBLEM ".out", "w", stdout);
+	freopen("uva" PROBLEM TESTC ".in", "r", stdin);
+	freopen("uva" PROBLEM ".out", "w", stdout);
 	#endif
+
+	USE_CPPIO();
+
+	string word;
+	int lengh;
+	int alphbat[30];
+
+	while( cin >> word >> lengh ){
+
+		int size,seat,i,j;
+		size = word.size();
+
+		memset(alphbat,0,sizeof(alphbat));
+
+		for( i = 0 ; i < size-1 ; i++ ){
+			for( j = i+1 ; j < size ; j++ ){
+				if( word[i] > word[j] )
+					swap(word[i],word[j]);
+			}
+		}
+
+		cout << word << endl;
+		// for( i = 0 ; i < size ; i++ ){
+		// 	seat = word[i]-'a';
+		// 	alphbat[seat]++;
+		// }
+
+
+
+		
+
+
+	}
+
 
 	return 0;
 }
---
+++ uva12908.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define TESTC ""
+#define PROBLEM "12908"
+
+#define USE_CPPIO() ios_base::sync_with_stdio(0); cin.tie(0)
+
+int data[15005],i;
+
+void setup(){
+	memset(data,0,sizeof(data));
+	for(i=1;i<15005;i++)
+		data[i]=data[i-1]+i;
+}
+
+int binary_search(int start,int end,int num){
+	int tmp = (start+end)/2;
+	if( (end-start) == 1 )
+		return end;
+
+	if( num > data[tmp] )
+		return binary_search(tmp,end,num);
+	else
+		return binary_search(start,tmp,num);
+}
+
+int main()
+{
+	#ifdef DBG
+	freopen("uva" PROBLEM TESTC ".in", "r", stdin);
+	freopen("uva" PROBLEM ".out", "w", stdout);
+	#endif
+
+	setup();
+
+	int number;
+	int seat;
+
+	while( ~scanf("%d",&number) && number ){
+		seat = binary_search(1,15005,number);
+		if( (data[seat]-number) == 0 ) seat++;
+		printf("%d %d\n",data[seat]-number,seat);
+	}
+
+	return 0;
+}
