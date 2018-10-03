503c678257531870bf8c3df193a6ca4d3af932a1
SunTalk <infernal56727@gmail.com>
2018-10-03 14:13:49 +0800

    update

--- cp.bat
+++ cp.bat
@@ -6,9 +6,7 @@ echo COMPILING...
 g++ uva%1.cpp -o uva%1.exe -DDBG
 
 echo FINISH
-
 if %ERRORLEVEL% EQU 0 (
 	uva%1.exe
 )
-
 popd
--- new.bat
+++ new.bat
@@ -7,5 +7,4 @@ type template > uva%1\uva%1.cpp
 echo.>uva%1\uva%1.in
 echo.>uva%1\uva%1.out
 
-
 echo uva%1 Created
