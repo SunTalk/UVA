8479c4ef158e9a041ee6898263590632c3c0c00a
SunTalk <infernal56727@gmail.com>
2018-10-16 11:54:39 +0800

    11352_notfinish_again

--- uva11352/uva11352.cpp
+++ uva11352/uva11352.cpp
@@ -8,109 +8,41 @@ using namespace std;
 #define USE_CPPIO() ios_base::sync_with_stdio(0); cin.tie(0)
 
 int Map[105][105];
-int check[105][105];
-int lengh,Min;
+queue <int> I;
+queue <int> J;
+// int End;
 
 void attack(int i,int j){
-	if( Map[i-2][j-1] == 0 )
+	if( Map[i-2][j-1] == 1000 )
 		Map[i-2][j-1] = 3;
-	if( Map[i-2][j+1] == 0 )
+	if( Map[i-2][j+1] == 1000 )
 		Map[i-2][j+1] = 3;
-	if( Map[i-1][j-2] == 0 )
+	if( Map[i-1][j-2] == 1000 )
 		Map[i-1][j-2] = 3;
-	if( Map[i-1][j+2] == 0 )
+	if( Map[i-1][j+2] == 1000 )
 		Map[i-1][j+2] = 3;
-	if( Map[i+1][j-2] == 0 )
+	if( Map[i+1][j-2] == 1000 )
 		Map[i+1][j-2] = 3;
-	if( Map[i+1][j+2] == 0 )
+	if( Map[i+1][j+2] == 1000 )
 		Map[i+1][j+2] = 3;
-	if( Map[i+2][j-1] == 0 )
+	if( Map[i+2][j-1] == 1000 )
 		Map[i+2][j-1] = 3;
-	if( Map[i+2][j+1] == 0 )
+	if( Map[i+2][j+1] == 1000 )
 		Map[i+2][j+1] = 3;
 }
 
-void Go(int i,int j,int lengh){
+void BFS(int i,int j,int lengh){
 
-	if( Map[i-1][j-1] == 200 )
-		if(lengh<Min)
-			Min = lengh;
-	if( Map[i-1][j] == 200 )
-		if(lengh<Min)
-			Min = lengh;
-	if( Map[i-1][j+1] == 200 )
-		if(lengh<Min)
-			Min = lengh;
-	if( Map[i][j-1] == 200 )
-		if(lengh<Min)
-			Min = lengh;
-	if( Map[i][j+1] == 200 )
-		if(lengh<Min)
-			Min = lengh;
-	if( Map[i+1][j-1] == 200 )
-		if(lengh<Min)
-			Min = lengh;
-	if( Map[i+1][j] == 200 )
-		if(lengh<Min)
-			Min = lengh;
-	if( Map[i+1][j+1] == 200 )
-		if(lengh<Min)
-			Min = lengh;
-
-	if( Map[i-1][j-1] == 0 && check[i-1][j-1] == 0 )
-		check[i-1][j-1] = 3;
-	if( Map[i-1][j] == 0 && check[i-1][j] == 0 )
-		check[i-1][j] = 3;
-	if( Map[i-1][j+1] == 0 && check[i-1][j+1] == 0 )
-		check[i-1][j+1] = 3;
-	if( Map[i][j-1] == 0 && check[i][j-1] == 0 )
-		check[i][j-1] = 3;
-	if( Map[i][j+1] == 0 && check[i][j+1] == 0 )
-		check[i][j+1] = 3;
-	if( Map[i+1][j-1] == 0 && check[i+1][j-1] == 0 )
-		check[i+1][j-1] = 3;
-	if( Map[i+1][j] == 0 && check[i+1][j] == 0 )
-		check[i+1][j] = 3;
-	if( Map[i+1][j+1] == 0 && check[i+1][j+1] == 0 )
-		check[i+1][j+1] = 3;
-	
-	//
-
-	if( check[i-1][j-1] == 0 )
-		check[i-1][j-1] = 1;
-	if( check[i-1][j] == 0 )
-		check[i-1][j] = 1;
-	if( check[i-1][j+1] == 0 )
-		check[i-1][j+1] = 1;
-	if( check[i][j-1] == 0 )
-		check[i][j-1] = 1;
-	if( check[i][j+1] == 0 )
-		check[i][j+1] = 1;
-	if( check[i+1][j-1] == 0 )
-		check[i+1][j-1] = 1;
-	if( check[i+1][j] == 0 )
-		check[i+1][j] = 1;
-	if( check[i+1][j+1] == 0 )
-		check[i+1][j+1] = 1;
-
-	//
-	if( check[i-1][j-1] == 3 )
-		Go(i-1,j-1,lengh+1);
-	if( check[i-1][j] == 3 )
-		Go(i-1,j,lengh+1);
-	if( check[i-1][j+1] == 3 )
-		Go(i-1,j+1,lengh+1);
-	if( check[i][j-1] == 3 )
-		Go(i,j-1,lengh+1);
-	if( check[i][j+1] == 3 )
-		Go(i,j+1,lengh+1);
-	if( check[i+1][j-1] == 3 )
-		Go(i+1,j-1,lengh+1);
-	if( check[i+1][j] == 3 )
-		Go(i+1,j,lengh+1);
-	if( check[i+1][j+1] == 3 )
-		Go(i+1,j+1,lengh+1);
-
+	printf("%d %d %d\n",i,j,lengh );
+	if( lengh <= Map[i-1][j-1] )
+	if( lengh <= Map[i-1][j] )
+	if( lengh <= Map[i-1][j+1] )
+	if( lengh <= Map[i][j-1] )
+	if( lengh <= Map[i][j+1] )
+	if( lengh <= Map[i+1][j-1] )
+	if( lengh <= Map[i+1][j] )
+	if( lengh <= Map[i+1][j+1] )
+		
 }
 
 int main(int argc, char const *argv[])
@@ -124,6 +56,7 @@ int main(int argc, char const *argv[])
 	scanf("%d",&times);
 	int line,column,j,k;
 	int king_i,king_j;
+	int ans_i,ans_j;
 	int ans;
 	char chess;
 
@@ -133,48 +66,75 @@ int main(int argc, char const *argv[])
 		getchar();
 
 		memset(Map,-1,sizeof(Map));
-		memset(check,0,sizeof(check));
-		lengh = 0;
 
 		for( j = 2 ; j <= line+1 ; j++ ){
 			for( k = 2 ; k <= column+1 ; k++ ){
 				scanf("%c",&chess);
 				if( chess == '.' )
-					Map[j][k] = 0;
+					Map[j][k] = 1000;
 				if( chess == 'A' ){
-					Map[j][k] = 100;
+					Map[j][k] = -1;
 					king_i = j;
 					king_j = k;
 				}
-				if( chess == 'B' )
-					Map[j][k] = 200;
+				if( chess == 'B' ){
+					Map[j][k] = 0;
+					ans_i = j;
+					ans_j = k;
+				}
 				if( chess == 'Z' )
-					Map[j][k] = 1;
+					Map[j][k] = -1;
 			}
 			getchar();
 		}
 
-		for( j = 2 ; j <= line+1 ; j++ ){
-			for( k = 2 ; k <= column+1 ; k++ ){
-				if( Map[j][k] == 1 )
-					attack(j,k);
-			}
-		}
-
-		lengh = 0;
-		Min = 2147482647;
-		Go(king_i,king_j,1);
-		ans = Min;
-
-		// for( j = 0 ; j <= line+3 ; j++ ){
-		// 	for( k = 0 ; k <= column+3 ; k++ ){
-		// 		printf("%4d",Map[j][k] );
+		// for( j = 2 ; j <= line+1 ; j++ ){
+		// 	for( k = 2 ; k <= column+1 ; k++ ){
+		// 		printf("%6d",Map[j][k] );
 		// 	}
 		// 	printf("\n");
 		// }
 		// printf("\n");
 
-		if( ans == 2147482647 )
+		for( j = 2 ; j <= line+1 ; j++ ){
+			for( k = 2 ; k <= column+1 ; k++ ){
+				if( Map[j][k] == -1 )
+					if( j != king_i ||  k != king_j )
+						attack(j,k);
+			}
+		}
+
+		for( j = 2 ; j <= line+1 ; j++ ){
+			for( k = 2 ; k <= column+1 ; k++ ){
+				if( Map[j][k] == 3 )
+					Map[j][k] = -1;
+					
+			}
+		}
+
+		Map[ans_i][ans_j] = 1000;
+
+		for( j = 2 ; j <= line+1 ; j++ ){
+			for( k = 2 ; k <= column+1 ; k++ ){
+				printf("%6d",Map[j][k] );
+			}
+			printf("\n");
+		}
+		printf("\n");
+
+		
+		
+		ans = Map[ans_i][ans_j];
+
+		for( j = 2 ; j <= line+1 ; j++ ){
+			for( k = 2 ; k <= column+1 ; k++ ){
+				printf("%6d",Map[j][k] );
+			}
+			printf("\n");
+		}
+		printf("\n");
+
+		if( ans == 1000 )
 			printf("King Peter, you can't go now!\n");
 		else
 			printf("Minimal possible length of a trip is %d\n",ans );
