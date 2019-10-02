7911e4f94e88d2bfe71c2b43eaa14986a7605b2f
SunTalk <infernal56727@gmail.com>
2019-10-02 13:07:52 +0800

    update script

--- cp.bat
+++ cp.bat
@@ -1,38 +1,39 @@
 @echo off
 
 if %1 leq 1000 (
-	goto hun
+	cls
+	echo COMPILING...
+	g++ -std=c++11 hundred\uva%1\uva%1.cpp -o hundred\uva%1\uva%1.exe
+
+	echo FINISH
+	if %ERRORLEVEL% EQU 0 (
+		hundred\uva%1\uva%1.exe < hundred\uva%1\uva%1.in > hundred\uva%1\uva%1.out
+	)
+	goto end
 )
 
 if %1 leq 10000 (
-	goto tho
+	cls
+	echo COMPILING...
+	g++ -std=c++11 thousand\uva%1\uva%1.cpp -o thousand\uva%1\uva%1.exe
+
+	echo FINISH
+	if %ERRORLEVEL% EQU 0 (
+		thousand\uva%1\uva%1.exe < thousand\uva%1\uva%1.in > thousand\uva%1\uva%1.out
+	)
+	goto end
 )
 
 if %1 leq 100000 (
-	goto mil
+	cls
+	echo COMPILING...
+	g++ -std=c++11 million\uva%1\uva%1.cpp -o million\uva%1\uva%1.exe
+
+	echo FINISH
+	if %ERRORLEVEL% EQU 0 (
+		million\uva%1\uva%1.exe < million\uva%1\uva%1.in > million\uva%1\uva%1.out
+	)
+	goto end
 )
 
-:hun
-	pushd hundred
-	goto run
-:tho
-	pushd thousand
-	goto run
-:mil
-	pushd million
-	goto run
-
-:run
-pushd uva%1
-
-cls
-echo COMPILING...
-g++ -std=c++11 uva%1.cpp -o uva%1.exe -DDBG
-
-echo FINISH
-if %ERRORLEVEL% EQU 0 (
-	uva%1.exe
-)
-
-popd
-popd
+:end
