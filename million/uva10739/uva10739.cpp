963d9e907b80cde5f946e20fbd039e47097a9a91
SunTalk <infernal56727@gmail.com>
2020-11-05 21:48:45 +0800

    update 10739

--- million/uva10739/uva10739.cpp
+++ million/uva10739/uva10739.cpp
@@ -2,49 +2,44 @@
 
 using namespace std;
 
-#define TESTC ""
-#define PROBLEM "10739"
-
 #define USE_CPPIO() ios_base::sync_with_stdio(0); cin.tie(0)
 #define MAXN 1000
+#define INF 0x3F3F3F3F
 
 int table[MAXN+5][MAXN+5];
+string str;
+
+int DP(int i, int j){
+
+	if( !(i<j) )
+		return table[i][j] = 0;
+
+	if( table[i][j] != INF )
+		return table[i][j];
+
+	if( str[i] == str[j] )
+		table[i][j] = min(table[i][j], DP(i+1, j-1));
+	
+	table[i][j] = min(table[i][j], DP(i+1, j-1)+1);
+	table[i][j] = min(table[i][j], DP(  i, j-1)+1);
+	table[i][j] = min(table[i][j], DP(i+1,   j)+1);
+
+	return table[i][j];
+
+}
 
 int main(int argc, char const *argv[])
 {
-	#ifdef DBG
-	freopen("uva" PROBLEM TESTC ".in", "r", stdin);
-	freopen("uva" PROBLEM ".out", "w", stdout);
-	#endif
 
-	int CASE;
-	scanf("%d",&CASE);
-
-	string str;
-	int cases = 1;
+	int kase;
+	scanf("%d",&kase);
 	
-	while( CASE-- ){
+	for(int K = 1 ; K <= kase ; K++ ){
 
 		cin >> str;
-		memset(table,0,sizeof(table));
+		memset(table,0x3F,sizeof(table));
+		printf("Case %d: %d\n", K, DP(0,str.size()-1));
 
-		for(int i = 0 ; i < str.size() ; i++ )
-			for(int j = 0 ; j < str.size() ; j++ )
-				table[i][j] = 0;
-
-		for(int j = 0 ; j < str.size()  ; j++ ){
-			for(int i = 0, k = j ; i < str.size(),k < str.size() ; i++,k++ ){				
-				
-				if( str[i] == str[k] )
-					table[i][k] = table[i+1][k-1];
-				else
-					table[i][k] = min(table[i+1][k-1],min(table[i][k-1],table[i+1][k])) + 1;
-					
-			}
-		}
-
-		printf("Case %d: %d\n",cases++, table[0][str.size()-1]);		
-	
 	}
 
 	return 0;
